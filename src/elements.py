51b03b18f19828f6484b94cc7d28edc6aba58a36
Nikos Karagiannakis <nikoskaragiannakis@gmail.com>
2015-08-23 20:38:20 +0100

    Changed images path

--- src/elements.py
+++ src/elements.py
@@ -12,9 +12,9 @@ COLOUR = {'red': (255, 0, 0),
           'green': (0, 255, 0),
           'black': (0, 0, 0),
           'grey': (180, 180, 180)}
-IMAGE = {'red': pg.image.load("images/red.png"),
-         'blue': pg.image.load("images/blue.png"),
-         'empty': pg.image.load("images/empty.png")}
+IMAGE = {'red': pg.image.load("../images/red.png"),
+         'blue': pg.image.load("../images/blue.png"),
+         'empty': pg.image.load("../images/empty.png")}
 OPERATORS = ['+', '-', '*', '//']
 NEIGHBOURS = {}
 BLANKER = pg.Surface((280, 30))
