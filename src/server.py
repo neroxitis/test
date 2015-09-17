70f6da14b0ab6d1a4203f802fb9f4e559d90d7b1
Nikos Karagiannakis <nikoskaragiannakis@gmail.com>
2015-09-17 20:23:53 +0100

    Players handled in the same way. #7

--- src/server.py
+++ src/server.py
@@ -30,34 +30,38 @@ class ServerChannel(Channel):
 
 class CalcuLinesServer(Server):
     channelClass = ServerChannel
+    player_colours = ["blue", "red"]
 
     def __init__(self, *args, **kwargs):
         self.id = 0
         Server.__init__(self, *args, **kwargs)
-        self.players = {}
+        self.no_players = kwargs['listeners']
         print 'CalcuLines Server launched!'
         self.game = None
         self.board = None
 
     def Connected(self, channel, addr):
-        print 'new connection:', channel
-        self.players[channel] = True
         if self.game is None:
             self.game = Game(channel)
-            self.game.redplayer.Send({"action": "hello",
-                                      "message": "Hello red player!",
-                                      "board": ""})
+        if len(self.game.players) < self.no_players:
+            print 'new connection:', channel
+            player_colour = self.player_colours.pop()
+            self.game.players[channel] = {'online': True,
+                                          'colour': player_colour}
+            channel.Send({"action": "hello",
+                              "message": "Welcome to CalcuLines! "
+                                         "You will be the " + player_colour +
+                                         " player.",
+                              "board": ""})
+
         else:
-            self.game.blueplayer = channel
-            self.game.blueplayer.Send({"action": "hello",
-                                       "message": "Hello blue player!",
-                                       "board": self.board})
-            self.game.redplayer.Send({"action": "startgame",
-                                      "player": "red",
-                                      "whoplays": "red"})
-            self.game.blueplayer.Send({"action": "startgame",
-                                       "player": "blue",
-                                       "whoplays": "red"})
+            # Need to dismiss more clients.
+            return
+        if len(self.game.players) == self.no_players:
+            for player, status in self.game.players.items():
+                player.Send({"action": "startgame",
+                             "player": status['colour'],
+                             "whoplays": "red"})
 
     def DeletePlayer(self, player):
         print 'player with id', str(player.address), 'has left the game.'
@@ -68,16 +72,12 @@ class CalcuLinesServer(Server):
         self.board = data['board']
         if update:
             print(data['whoplays'], data['score_info'])
-            self.game.redplayer.Send({"action": "update",
-                                      "board": data['board'],
-                                      "whoplays": data['whoplays'],
-                                      "score_info": data['score_info'],
-                                      "no_cells": data['no_cells']})
-            self.game.blueplayer.Send({"action": "update",
-                                       "board": data['board'],
-                                      "whoplays": data['whoplays'],
-                                      "score_info": data['score_info'],
-                                      "no_cells": data['no_cells']})
+            for player in self.game.players:
+                player.Send({"action": "update",
+                             "board": data['board'],
+                             "whoplays": data['whoplays'],
+                             "score_info": data['score_info'],
+                             "no_cells": data['no_cells']})
 
     def Launch(self):
         while True:
@@ -89,14 +89,16 @@ class Game:
     def __init__(self, redplayer):
         # 'red' or 'blue'
         self.turn = 'red'
-        self.redplayer = redplayer
-        self.blueplayer = None
+        self.players = {}
 
 # Get command line argument of server, port
-if len(sys.argv) != 2:
-    print "Usage:", sys.argv[0], "host:port"
-    print "e.g.", sys.argv[0], "localhost:12345"
+if len(sys.argv) != 3:
+    print "Usage:", sys.argv[0], "host:port players:number"
+    print "e.g.", sys.argv[0], "localhost:12345 players"
+    print ""
 else:
     host, port = sys.argv[1].split(":")
-    server = CalcuLinesServer(localaddr=(host, int(port)))
+    no_players = int(sys.argv[2].split(":")[1])
+    server = CalcuLinesServer(localaddr=(host, int(port)),
+                              listeners=no_players)
     server.Launch()
